2e6f5ccef04e5148ef58be3be6d69814bada3017
eknoor555 <83641013+eknoor555@users.noreply.github.com>
2021-05-26 23:38:58 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,sum;
+    printf("Enter the 1st no");
+    scanf("%d",&a);
+    printf("Enter the 2nd no");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("\n%d",sum);
+    return 0;
+}
+
+
